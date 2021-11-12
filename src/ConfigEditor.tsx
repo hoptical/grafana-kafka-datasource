e1705abfb2dfbfbe50ca18480ce33fb7f68eab96
mortexa <seyed.me720@gmail.com>
2021-11-12 18:19:03 +0330

    improve the datasource config editor

--- src/ConfigEditor.tsx
+++ src/ConfigEditor.tsx
@@ -1,24 +1,33 @@
 import React, { ChangeEvent, PureComponent } from 'react';
-import { LegacyForms } from '@grafana/ui';
-import { DataSourcePluginOptionsEditorProps } from '@grafana/data';
-import { MyDataSourceOptions, MySecureJsonData } from './types';
+import { LegacyForms, InlineFormLabel } from '@grafana/ui';
+import { DataSourcePluginOptionsEditorProps, SelectableValue } from '@grafana/data';
+import { KafkaOptions, KafkaSecureJsonData, AutoOffsetReset } from './types';
 
-const { SecretFormField, FormField } = LegacyForms;
+const { SecretFormField, FormField, Select } = LegacyForms;
 
-interface Props extends DataSourcePluginOptionsEditorProps<MyDataSourceOptions> {}
+const autoResetOffsets = [
+  {
+    label: 'Earliest',
+    value: AutoOffsetReset.EARLIEST,
+    description: 'Automatically reset the offset to the earliest offset'
+  },
+  {
+    label: 'Latest',
+    value: AutoOffsetReset.LATEST,
+    description: 'Automatically reset the offset to the latest offset'
+  },
+  {
+    label: 'None',
+    value: AutoOffsetReset.NONE,
+    description: 'Throw exception to the consumer if no previous offset is found for the consumer\'s group'
+  },
+] as Array<SelectableValue<AutoOffsetReset>>;
 
-interface State {}
+interface Props extends DataSourcePluginOptionsEditorProps<KafkaOptions> { }
+
+interface State { }
 
 export class ConfigEditor extends PureComponent<Props, State> {
-  //const {bootstrapServers} = jsonData;
-  // onPathChange = (event: ChangeEvent<HTMLInputElement>) => {
-  //   const { onOptionsChange, options } = this.props;
-  //   const jsonData = {
-  //     ...options.jsonData,
-  //     path: event.target.value,
-  //   };
-  //   onOptionsChange({ ...options, jsonData });
-  // };
 
   // Secure field (only sent to the backend)
   onAPIKeyChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -45,14 +54,7 @@ export class ConfigEditor extends PureComponent<Props, State> {
       },
     });
   };
-  // onURLChange = (event: ChangeEvent<HTMLInputElement>) => {
-  //   const { onOptionsChange, options } = this.props;
-  //   const jsonData = {
-  //     ...options.jsonData,
-  //     URLs: event.target.value,
-  //   };
-  //   onOptionsChange({ ...options, jsonData });
-  // };
+
   onBootstrapServersChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { onOptionsChange, options } = this.props;
     const jsonData = {
@@ -61,37 +63,81 @@ export class ConfigEditor extends PureComponent<Props, State> {
     };
     onOptionsChange({ ...options, jsonData });
   };
+
+  onAutoResetOffsetChanged = (selected: SelectableValue<AutoOffsetReset>) => {
+    const { options, onOptionsChange } = this.props;
+
+    const jsonData = {
+      ...options.jsonData,
+      autoOffsetReset: selected.value || AutoOffsetReset.EARLIEST,
+    };
+    onOptionsChange({ ...options, jsonData });
+  };
+
+
+  resolveAutoResetOffset = (value: string | undefined) => {
+    if (value === AutoOffsetReset.LATEST) {
+      return autoResetOffsets[1];
+    }
+    if (value === AutoOffsetReset.NONE) {
+      return autoResetOffsets[3];
+    }
+    return autoResetOffsets[0];
+  };
+
   render() {
     const { options } = this.props;
     const { jsonData, secureJsonFields } = options;
-    const secureJsonData = (options.secureJsonData || {}) as MySecureJsonData;
+    const secureJsonData = (options.secureJsonData || {}) as KafkaSecureJsonData;
 
     return (
-      <div className="gf-form-group">
-        <div className="gf-form">
-          <FormField
-            label="Servers"
-            onChange={this.onBootstrapServersChange}
-            value={jsonData.bootstrapServers || ''}
-            placeholder="broker1:9092, broker2:9092"
-          />
-        </div>
-
-        <div className="gf-form-inline">
+      <>
+        <h3 className="page-heading">Kafka connections</h3>
+        <div className="gf-form-group">
           <div className="gf-form">
-            <SecretFormField
-              isConfigured={(secureJsonFields && secureJsonFields.apiKey) as boolean}
-              value={secureJsonData.apiKey || ''}
-              label="API Key"
-              placeholder="secure json field (backend only)"
-              labelWidth={6}
-              inputWidth={20}
-              onReset={this.onResetAPIKey}
-              onChange={this.onAPIKeyChange}
+            <FormField
+              label="Bootstrap Servers"
+              onChange={this.onBootstrapServersChange}
+              value={jsonData.bootstrapServers || ''}
+              placeholder="broker1:9092, broker2:9092"
+              labelWidth={10}
+              inputWidth={15}
+            />
+          </div>
+          <div className="gf-form">
+            <InlineFormLabel
+              className="width-10"
+              tooltip="What to do when there is no initial offset in Kafka or if the current offset does not exist any more on the server (e.g. because that data has been deleted)"
+            >
+              Auto offset reset
+            </InlineFormLabel>
+            <Select
+              className="width-15"
+              value={this.resolveAutoResetOffset(jsonData.autoOffsetReset)}
+              options={autoResetOffsets}
+              defaultValue={autoResetOffsets[0]}
+              onChange={this.onAutoResetOffsetChanged}
             />
           </div>
         </div>
-      </div>
+        <h3 className="page-heading">Auth</h3>
+        <div className="gf-form-group">
+          <div className="gf-form-inline">
+            <div className="gf-form">
+              <SecretFormField
+                isConfigured={(secureJsonFields && secureJsonFields.apiKey) as boolean}
+                value={secureJsonData.apiKey || ''}
+                label="API Key"
+                placeholder="secure json field (backend only)"
+                labelWidth={6}
+                inputWidth={20}
+                onReset={this.onResetAPIKey}
+                onChange={this.onAPIKeyChange}
+              />
+            </div>
+          </div>
+        </div>
+      </>
     );
   }
 }
