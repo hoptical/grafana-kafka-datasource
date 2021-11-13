d7beb143b48ee2841775700e1eb5e84ef187bc5e
karbasi <hamed.karbasi.93@gmail.com>
2021-11-14 02:47:41 +0330

    Revert "[refactor] use `handleEvent`"
    This reverts commit 64c34eb3486696c4141cb4d7f021616c0441ede2.

--- src/QueryEditor.tsx
+++ src/QueryEditor.tsx
@@ -1,58 +1,61 @@
 import { defaults } from 'lodash';
-import React from 'react';
-import { Form, Field, Input, Switch } from '@grafana/ui';
+import React, { ChangeEvent, PureComponent, SyntheticEvent } from 'react';
+import { LegacyForms } from '@grafana/ui';
 import { QueryEditorProps } from '@grafana/data';
 import { DataSource } from './datasource';
 import { defaultQuery, KafkaDataSourceOptions, KafkaQuery } from './types';
-import { handlerFactory } from 'handleEvent';
+
+const { FormField, Switch } = LegacyForms;
 
 type Props = QueryEditorProps<DataSource, KafkaQuery, KafkaDataSourceOptions>;
 
-export const QueryEditor = (props: Props) => {
-  const { onChange } = props;
-  const query = defaults(props.query, defaultQuery);
-  const handleEvent = handlerFactory(query, onChange);
-  const { topicName, partition, withStreaming } = query;
+export class QueryEditor extends PureComponent<Props> {
+  onTopicNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { onChange, query, onRunQuery } = this.props;
+    onChange({ ...query, topicName: event.target.value });
+    onRunQuery();
+  };
 
-  return (
-    <Form onSubmit={() => { }}>
-      {() => (
-        <div className="gf-form">
-          <div className="m-r-1 m-t-1">
-            <Field label="Topic Name">
-              <Input
-                name="topicName"
-                value={topicName || ''}
-                onChange={handleEvent('topicName')}
-                type="text"
-                css=""
-                autoComplete="off"
-                required
-              />
-            </Field>
-          </div>
-          <div className="m-r-1 m-t-1">
-            <Field label="Partition">
-              <Input
-                name="partition"
-                value={partition}
-                onChange={handleEvent('partition')}
-                type="number"
-                step="1"
-                min="0"
-                css=""
-                autoComplete="off"
-                required
-              />
-            </Field>
-          </div>
-          <div className="m-r-1 m-t-1">
-            <Field label="Enable streaming (v8+)">
-              <Switch name="withStreaming" checked={withStreaming || false} onChange={handleEvent('withStreaming')} css="" />
-            </Field>
-          </div>
-        </div>
-      )}
-    </Form>
-  );
+  onPartitionChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { onChange, query, onRunQuery } = this.props;
+    onChange({ ...query, partition: parseFloat(event.target.value) });
+    onRunQuery();
+  };
+
+  onWithStreamingChange = (event: SyntheticEvent<HTMLInputElement>) => {
+    const { onChange, query, onRunQuery } = this.props;
+    onChange({ ...query, withStreaming: event.currentTarget.checked });
+    onRunQuery();
+  };
+
+  render() {
+    const query = defaults(this.props.query, defaultQuery);
+    const { topicName, partition, withStreaming } = query;
+
+    return (
+      <div className="gf-form">
+        <FormField
+          labelWidth={7}
+          width={8}
+          value={topicName || ''}
+          onChange={this.onTopicNameChange}
+          label="Topic Name"
+          type="text"
+        />
+
+        <FormField
+          labelWidth={5}
+          width={4}
+          value={partition}
+          onChange={this.onPartitionChange}
+          label="partition"
+          type="number"
+          step="1"
+          min="0"
+        />
+
+        <Switch checked={withStreaming || false} label="Enable streaming (v8+)" onChange={this.onWithStreamingChange} />
+      </div>
+    );
+  }
 }
