1e3c7214cfdf7a7767f649bb24338b4e41cec383
karbasi <hamed.karbasi.93@gmail.com>
2021-11-18 02:38:08 +0330

    add autoOffsetReset support to react

--- src/QueryEditor.tsx
+++ src/QueryEditor.tsx
@@ -1,11 +1,24 @@
 import { defaults } from 'lodash';
 import React, { ChangeEvent, PureComponent, SyntheticEvent } from 'react';
-import { LegacyForms } from '@grafana/ui';
-import { QueryEditorProps } from '@grafana/data';
+import { LegacyForms, InlineFormLabel } from '@grafana/ui';
+import { QueryEditorProps, SelectableValue } from '@grafana/data';
 import { DataSource } from './datasource';
-import { defaultQuery, KafkaDataSourceOptions, KafkaQuery } from './types';
+import { defaultQuery, KafkaDataSourceOptions, KafkaQuery, AutoOffsetReset } from './types';
 
-const { FormField, Switch } = LegacyForms;
+const { FormField, Switch, Select } = LegacyForms;
+
+const autoResetOffsets = [
+  {
+    label: 'From the last 100',
+    value: AutoOffsetReset.EARLIEST,
+    description: 'Consume from the last 100 offset',
+  },
+  {
+    label: 'Latest',
+    value: AutoOffsetReset.LATEST,
+    description: 'Consume from the latest offset',
+  },
+] as Array<SelectableValue<AutoOffsetReset>>;
 
 type Props = QueryEditorProps<DataSource, KafkaQuery, KafkaDataSourceOptions>;
 
@@ -28,9 +41,21 @@ export class QueryEditor extends PureComponent<Props> {
     onRunQuery();
   };
 
+  onAutoResetOffsetChanged = (selected: SelectableValue<AutoOffsetReset>) => {
+    const { onChange, query, onRunQuery } = this.props;
+    onChange({ ...query, autoOffsetReset: selected.value || AutoOffsetReset.LATEST });
+    onRunQuery();
+  };
+
+  resolveAutoResetOffset = (value: string | undefined) => {
+    if (value === AutoOffsetReset.LATEST) {
+      return autoResetOffsets[1];
+    }
+    return autoResetOffsets[0];
+  };
   render() {
     const query = defaults(this.props.query, defaultQuery);
-    const { topicName, partition, withStreaming } = query;
+    const { topicName, partition, withStreaming, autoOffsetReset } = query;
 
     return (
       <div className="gf-form">
@@ -39,7 +64,7 @@ export class QueryEditor extends PureComponent<Props> {
           width={8}
           value={topicName || ''}
           onChange={this.onTopicNameChange}
-          label="Topic Name"
+          label="Topic"
           type="text"
         />
 
@@ -54,6 +79,21 @@ export class QueryEditor extends PureComponent<Props> {
           min="0"
         />
 
+        <div className="gf-form">
+          <InlineFormLabel
+            className="width-5"
+            tooltip="Starting offset to consume that can be from latest or last 100."
+          >
+            Auto offset reset
+          </InlineFormLabel>
+          <Select
+            className="width-10"
+            value={this.resolveAutoResetOffset(autoOffsetReset)}
+            options={autoResetOffsets}
+            defaultValue={autoResetOffsets[0]}
+            onChange={this.onAutoResetOffsetChanged}
+          />
+        </div>
         <Switch checked={withStreaming || false} label="Enable streaming (v8+)" onChange={this.onWithStreamingChange} />
       </div>
     );
--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,24 @@
 import { DataQuery, DataSourceJsonData } from '@grafana/data';
 
+export enum AutoOffsetReset {
+  EARLIEST = 'earliest',
+  LATEST = 'latest',
+}
 export interface KafkaQuery extends DataQuery {
   topicName: string;
   partition: number;
   withStreaming: boolean;
+  autoOffsetReset: AutoOffsetReset;
 }
 
 export const defaultQuery: Partial<KafkaQuery> = {
   partition: 0,
   withStreaming: true,
+  autoOffsetReset: AutoOffsetReset.LATEST,
+};
+
+export type AutoOffsetResetInterface = {
+  [key in AutoOffsetReset]: string;
 };
 
 export interface KafkaDataSourceOptions extends DataSourceJsonData {
