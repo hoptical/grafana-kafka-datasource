727e9aba032e2a3a62cf6c75135ac062b3250fcf
Hamed Karbasi <hamed.karbasi.93@gmail.com>
2025-04-29 13:45:37 +0200

    add template variables and filter query to frontend

--- src/datasource.ts
+++ src/datasource.ts
@@ -1,26 +1,59 @@
-import { DataQueryRequest, DataQueryResponse, DataSourceInstanceSettings, LiveChannelScope } from '@grafana/data';
-import { DataSourceWithBackend, getGrafanaLiveSrv } from '@grafana/runtime';
-import { Observable, merge } from 'rxjs';
-import { KafkaDataSourceOptions, KafkaQuery } from './types';
-
+import { CoreApp, DataQueryRequest, DataQueryResponse, DataSourceInstanceSettings, LiveChannelScope, ScopedVars } from '@grafana/data';
+import { DataSourceWithBackend, getGrafanaLiveSrv, getTemplateSrv } from '@grafana/runtime';
+import { Observable, merge, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { KafkaDataSourceOptions, KafkaQuery, AutoOffsetReset, TimestampMode } from './types';
 export class DataSource extends DataSourceWithBackend<KafkaQuery, KafkaDataSourceOptions> {
   constructor(instanceSettings: DataSourceInstanceSettings<KafkaDataSourceOptions>) {
     super(instanceSettings);
   }
-  query(request: DataQueryRequest<KafkaQuery>): Observable<DataQueryResponse> {
-    const observables = request.targets.map((query, index) => {
 
-      return getGrafanaLiveSrv().getDataStream({
-        addr: {
-          scope: LiveChannelScope.DataSource,
-          namespace: this.uid,
-          path: `${query.topicName}-${query.partition}-${query.autoOffsetReset}`, // this will allow each new query to create a new connection
-          data: {
-            ...query,
+  getDefaultQuery(_: CoreApp): Partial<KafkaQuery> {
+    return {
+      topicName: '',
+      partition: 0,
+      withStreaming: true,
+      autoOffsetReset: AutoOffsetReset.LATEST,
+      timestampMode: TimestampMode.Now
+    };
+  }
+
+  filterQuery(query: KafkaQuery): boolean {
+    return !!query.topicName && query.partition >= 0;
+  }
+
+  applyTemplateVariables(query: KafkaQuery, scopedVars: ScopedVars) {
+    const templateSrv = getTemplateSrv();
+    return {
+      ...query,
+      topicName: templateSrv.replace(query.topicName, scopedVars),
+      partition: parseInt(templateSrv.replace(query.partition.toString(), scopedVars), 10),
+    };
+  }
+
+  query(request: DataQueryRequest<KafkaQuery>): Observable<DataQueryResponse> {
+    const observables = request.targets
+      .filter(this.filterQuery)
+      .map(query => {
+        const interpolatedQuery = this.applyTemplateVariables(query, request.scopedVars);
+        
+        return getGrafanaLiveSrv().getDataStream({
+          addr: {
+            scope: LiveChannelScope.DataSource,
+            namespace: this.uid,
+            path: `${interpolatedQuery.topicName}-${interpolatedQuery.partition}-${interpolatedQuery.autoOffsetReset}`,
+            data: interpolatedQuery,
           },
-        },
+        }).pipe(
+          catchError(err => {
+            console.error('Stream error:', err);
+            return throwError(() => ({
+              message: `Error connecting to Kafka topic ${interpolatedQuery.topicName}: ${err.message}`,
+              status: 'error'
+            }));
+          })
+        );
       });
-    });
 
     return merge(...observables);
   }
