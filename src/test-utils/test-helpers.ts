a45bc16c60b8f2fc3fd85d13310bed4ff42ed9f5
Hamed Karbasi <hamed.karbasi.93@gmail.com>
2025-09-06 19:26:52 +0000

    refactor: optimize deepFreeze utility function

--- src/test-utils/test-helpers.ts
+++ src/test-utils/test-helpers.ts
@@ -8,11 +8,26 @@
  * @returns The deep frozen object
  */
 export const deepFreeze = (obj: any): any => {
+  // Return immediately for null, undefined, or non-objects
+  if (obj === null || typeof obj !== 'object') {
+    return obj;
+  }
+  
+  // Return immediately if already frozen
+  if (Object.isFrozen(obj)) {
+    return obj;
+  }
+  
+  // Freeze the object
   Object.freeze(obj);
+  
+  // Recursively deep freeze own properties that are objects and not already frozen
   Object.getOwnPropertyNames(obj).forEach(prop => {
-    if (obj[prop] !== null && typeof obj[prop] === 'object' && !Object.isFrozen(obj[prop])) {
-      deepFreeze(obj[prop]);
+    const propValue = obj[prop];
+    if (propValue !== null && typeof propValue === 'object' && !Object.isFrozen(propValue)) {
+      deepFreeze(propValue);
     }
   });
+  
   return obj;
 };
